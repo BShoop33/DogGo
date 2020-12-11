e906d98b3a833491a2a18e5e0114033ef8ccdca9
Braxton Shoop <braxton.shoop@gmail.com>
2020-12-11 13:39:21 -0600

    exercise 1 completed

---
+++ DogGo/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DogGo.Models
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+
+        Owner GetOwnerById(int id);
+    }
+}
